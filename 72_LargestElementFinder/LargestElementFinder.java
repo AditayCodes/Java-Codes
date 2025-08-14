08148f37367c64907c3f87c983b8d481c94fc694
ADITAY SHARMA <137498537+AditaySharma18@users.noreply.github.com>
2025-08-14 23:57:33 +0530

    array part-1

---
+++ 72_LargestElementFinder/LargestElementFinder.java
@@ -0,0 +1,41 @@
+//Program for finding the largest element of the array
+
+import java.util.Scanner;
+
+public class LargestElementFinder {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input array size
+        System.out.print("Enter the number of elements in the array: ");
+        int n = sc.nextInt();
+
+        // Validate size
+        if (n <= 0) {
+            System.out.println("Array size must be greater than 0.");
+            sc.close();
+            return;
+        }
+
+        int[] arr = new int[n];
+
+        // Input array elements
+        System.out.println("Enter " + n + " integers:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // Find the largest element
+        int max = arr[0];
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        // Output the result
+        System.out.println("The largest element in the array is: " + max);
+
+        sc.close(); 
+    }
+}
